d0b521e95ebf44a72824e4115dc631ec8a64417a
Servbt <arianrezvani00@gmail.com>
2022-07-06 13:47:17 -0400

    got omdb api linked up to js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,17 @@
 console.log("please work");
+
+// var apiUrl = "a7b45c21"
+
+var getOMDB = function () {
+    var omdbLink = " http://www.omdbapi.com/?i=tt3896198&apikey=a7b45c21"
+    fetch(omdbLink)
+    .then(function (response) {
+        console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        
+    })};
+
+getOMDB();
