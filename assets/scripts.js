4faeaf98bc7fbb9dbea1d8a236e1e85aed0c2bdb
kmart134 <ksmgps@hotmail.com>
2022-07-07 07:07:48 -0400

    added eventListener to button

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,18 @@
-console.log("please work");
+var button = document.querySelector("#heroButton");
+var searchText = document.querySelector("#search-input");
+
+var searchHandle = function (event){
+    event.preventDefault();
+    var search= searchText.value.trim()
+    console.log(search);
+    console.log("working");
+}
+
+
+
+
+
+button.addEventListener('click', searchHandle);
+
+
+
