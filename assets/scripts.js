ee0cbbcfbced8fa79222bbbb7816ae181b5ea1f9
Servbt <arianrezvani00@gmail.com>
2022-07-07 11:39:35 -0400

    fixed ombd search result and logged it out

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,8 +6,25 @@ var searchHandle = function (event){
     var search= searchText.value.trim()
     console.log(search);
     console.log("working");
+    getOMDB(search)
 }
 
+
+
+
+// var apiUrl = "a7b45c21"
+
+var getOMDB = function (search) {
+    var omdbLink = `http://www.omdbapi.com/?t=${search}&apikey=a7b45c21`
+    fetch(omdbLink)
+    .then(function (response) {
+        console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        
+    })};
 
 
 
