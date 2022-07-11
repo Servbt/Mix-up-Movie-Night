3659610589a31de24e79a3034c808a7a1e4f9916
khaledghanem1 <khaledghanim96@gmail.com>
2022-07-11 10:23:10 -0600

    Getting drinks based on user click

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   //var button = document.querySelector("#heroButton");
   //var searchText = document.querySelector("#search-input");
   var tileTags = document.querySelectorAll(".tile .box");
+  var chooseNonAlcoholic;
+  var chooseOrdinaryDrink;
+  var chooseFancyCocktails;
 
   /*   var movieGenres = [
     "Action",
@@ -38,14 +41,15 @@ $(document).ready(function () {
   var animation = ["Madagascar", "Finding Nemo", "Minions", "Despicable Me"];
   var comedy = ["Home Alone", "21 Jump Street", "Ghostbusters"];
 
-  /* LOOP THROUGH TILES + GET GENRE FROM CLICK */
+  /* LOOP THROUGH TILES + GET GENRE + DRINK FROM CLICK */
   $(tileTags).each(function () {
     $(this).click(function () {
       // console.log(this.find('.title').text())
-      var genreClicked = $(this).find(".title").text();
-      console.log(genreClicked);
+      var btnClicked = $(this).find(".title").text();
+      console.log(btnClicked);
       /* console.log("this was clicked" + $(this).text() + index) */
-      genreClick(genreClicked);
+      genreClick(btnClicked);
+      drinkClicked(btnClicked)
     });
   });
 
@@ -70,6 +74,15 @@ $(document).ready(function () {
       getOMDB(randomAnimation);
     }
   };
+ var drinkClicked = function(drinksClicked) {
+  console.log("this worked" + drinksClicked);
+  if (drinksClicked === "Non Alcoholic Drinks") {
+    chooseNonAlcoholic = true;
+    drinkOptions();
+    console.log(chooseNonAlcoholic)
+  } 
+
+ }
 
   /*   $("#heroButton").click(function () {
     console.log("this was clicked");
@@ -101,10 +114,6 @@ $(document).ready(function () {
   };
 
   /* button.addEventListener('click', searchHandle); */
-
-  var chooseNonAlcoholic = true;
-  var chooseOrdinaryDrink = true;
-  var chooseFancyCocktails = true;
 
   // GATHERING DRINK OPTIONS
   function drinkOptions() {
@@ -152,5 +161,6 @@ $(document).ready(function () {
         });
     }
   }
-  drinkOptions();
+  
+  
 });
